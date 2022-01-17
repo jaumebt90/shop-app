79524fc75e9e0a4bb3c6c0da51842f5dd32af94b
Jaume <ballestertomasj@gmail.com>
2022-01-17 18:23:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+class App extends Component {
+  state = {
+    productos: [
+      { name: "Tomate", price: 1500, img: "/productos/tomate.jpg" },
+      { name: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
+      { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    );
+  }
+}
 
 export default App;
