8cac226faefab5c202d341e225a678f0c62e8e8f
Jaume <ballestertomasj@gmail.com>
2022-01-18 09:53:42 +0100

    layout

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,10 @@ const styles = {
   button: {
     backgroundColor: "#0A283E",
     color: "#fff",
+    padding: "15px 20px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
   },
 };
 
