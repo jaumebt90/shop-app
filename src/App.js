9ba87d21f16f71280b3c2a2a5661f42f93f3bccc
Jaume <ballestertomasj@gmail.com>
2022-01-18 12:03:01 +0100

    bubble

--- src/App.js
+++ src/App.js
@@ -37,10 +37,10 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.state.carro);
+    //console.log(this.state.carro);
     return (
       <div>
-        <Navbar />
+        <Navbar carro={this.state.carro} />
         <Layout>
           <Title />
           <Productos
