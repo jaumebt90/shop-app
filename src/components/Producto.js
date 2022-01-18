8cdc4a1f5ec10f70b2c8c93ecc4f0f3382da3274
Jaume <ballestertomasj@gmail.com>
2022-01-18 09:23:51 +0100

    Update Producto.js

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -4,6 +4,12 @@ const styles = {
   producto: {
     border: "solid 1px #eee",
     boxShadow: "0 5px 5px rgb(0,0,0,0.1)",
+    width: "30%",
+    padding: "10px 15px",
+    borderRadius: "5px",
+  },
+  img: {
+    width: "100%",
   },
 };
 
@@ -13,7 +19,7 @@ class Producto extends Component {
     const { producto } = this.props;
     return (
       <div style={styles.producto}>
-        <img alt={producto.name} src={producto.img} />
+        <img style={styles.img} alt={producto.name} src={producto.img} />
         <h3>{producto.name}</h3>
         <p>{producto.price}</p>
       </div>
