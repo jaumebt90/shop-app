ecb978638f2dae7717b74253fd39f0aa19640b83
Jaume <ballestertomasj@gmail.com>
2022-01-18 09:15:23 +0100

    Update Producto.js

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -7,6 +7,8 @@ class Producto extends Component {
     return (
       <div>
         <img alt={producto.name} src={producto.img} />
+        <h3>{producto.name}</h3>
+        <p>{producto.price}</p>
       </div>
     );
   }
