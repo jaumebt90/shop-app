04da31f505edd6599497adf310b3e0e7e9708107
Jaume <ballestertomasj@gmail.com>
2022-01-18 09:21:15 +0100

    Update Producto.js

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,11 +1,18 @@
 import { Component } from "react";
 
+const styles = {
+  producto: {
+    border: "solid 1px #eee",
+    boxShadow: "0 5px 5px rgb(0,0,0,0.1)",
+  },
+};
+
 class Producto extends Component {
   render() {
     //console.log(this.props);
     const { producto } = this.props;
     return (
-      <div>
+      <div style={styles.producto}>
         <img alt={producto.name} src={producto.img} />
         <h3>{producto.name}</h3>
         <p>{producto.price}</p>
